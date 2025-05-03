def700effdda7b9e6c8169e0a57804045d2f1d60
Odu-Enkay <[erozonankiru@gmail.com]>
2025-05-03 17:48:46 -0600

    improves addProperty function to connect with db

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -120,8 +120,8 @@ const getAllProperties = function (options, limit = 10) {
  */
 const addProperty = function (property) {
   return pool
-  .query(`INSERT INTO properties(title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
-  VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *;`, [property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.country, property.street, property.city, property.province, property.post_code, property.actived])
+  .query(`INSERT INTO properties(owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code)
+  VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *;`, [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.country, property.street, property.city, property.province, property.post_code])
   .then((result) => {
     console.log(result.rows);
     return result.rows[0];
